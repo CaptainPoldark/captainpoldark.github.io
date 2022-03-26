4af2baaec8c1e09af188c29ebda1a1bee631a281
CaptainPoldark <61985921+CaptainPoldark@users.noreply.github.com>
2022-03-26 01:46:31 -0400

    Added projects section
    Added projects section, and pulled projects from a JSON file. Added styling to the section and cards

---
+++ src/components/Projects/ProjectCards.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import { Container, Col, Row, Card, Button } from "react-bootstrap";
+import ProjectList from "./project-list.json";
+
+function ProjectCards() {
+  console.log(ProjectList.projects.length);
+
+  /*   const createCards = (project) => {
+    return (
+      <Card className="project-container-card">
+        <Card.Img variant="top" src={project.links.image} />
+        <Card.Body>
+          <Card.Title>{project.title}</Card.Title>
+          <Card.Text style={{ textAlign: "justify" }}>
+            {project.description}
+            <br />
+            <br />
+            <p className="project-card-tech">Made With:</p>
+            {project.tech.map((tech) => {
+              return `${tech}, `;
+            })}
+          </Card.Text>
+          <Button variant="primary" href={project.links.demo}>
+            Demo
+          </Button>
+        </Card.Body>
+      </Card>
+    );
+  }; */
+  return (
+    <Container fluid>
+      <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+        {ProjectList.projects.map((project) => {
+          return (
+            <Col md={4} className="project-container-card">
+              <Card className="project-card-card">
+                <Card.Img variant="top" src={project.links.image} />
+                <Card.Body>
+                  <Card.Title>{project.title}</Card.Title>
+                  <Card.Text className="project-card-text">
+                    {project.description}
+                    <br />
+                    <br />
+                    <p className="project-card-tech">Made With:</p>
+                    <Row>
+                      {project.tech.map((tech) => {
+                        return <Col>{tech}</Col>;
+                      })}
+                    </Row>
+                  </Card.Text>
+                  <Button variant="primary" href={project.links.demo}>
+                    Demo
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
+}
+
+export default ProjectCards;
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCards from "./ProjectCards";
 
 function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div className="projects-section">
+      <h1 className="highlight">Current Projects</h1>
+      <p>
+        Listed below are some of the projects I'm currently working. You can
+        find more on my GitHub porfolio.
+      </p>
+
+      <ProjectCards />
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
